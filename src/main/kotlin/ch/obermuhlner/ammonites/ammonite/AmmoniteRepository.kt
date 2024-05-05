a68e27113343dbad3f53ba1401bd44f8fb66c4f5
Eric Obermühlner <eobermuhlner@gmail.com>
2024-05-05 16:00:34 +0200

    add AmmoniteRepository

---
+++ src/main/kotlin/ch/obermuhlner/ammonites/ammonite/AmmoniteRepository.kt
@@ -0,0 +1,65 @@
+package ch.obermuhlner.ammonites.ammonite
+
+import ch.obermuhlner.ammonites.jooq.Tables
+import ch.obermuhlner.ammonites.jooq.Tables.AMMONITE
+import ch.obermuhlner.ammonites.jooq.tables.pojos.Ammonite
+import ch.obermuhlner.ammonites.jooq.tables.pojos.Measurement
+import org.jooq.DSLContext
+import org.jooq.Record
+import org.jooq.Result
+import org.jooq.impl.DSL
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+class AmmoniteRepository(private val dsl: DSLContext) {
+
+    fun findAll(): List<Ammonite> {
+        return dsl.selectFrom(AMMONITE).fetchInto(Ammonite::class.java)
+    }
+
+    fun findById(id: Int): Optional<Ammonite> {
+        return dsl.selectFrom(AMMONITE).where(AMMONITE.ID.eq(id)).fetchOptionalInto(Ammonite::class.java)
+    }
+
+    fun save(ammonite: Ammonite): Ammonite {
+        if (ammonite.id == null) {
+            val record = dsl.newRecord(AMMONITE, ammonite)
+            record.store()
+            return record.into(Ammonite::class.java)
+        } else {
+            // Update existing record
+            dsl.update(AMMONITE)
+                .set(AMMONITE.TAXONOMY_SUBCLASS, ammonite.taxonomySubclass)
+                .set(AMMONITE.TAXONOMY_FAMILY, ammonite.taxonomyFamily)
+                .set(AMMONITE.TAXONOMY_SUBFAMILY, ammonite.taxonomySubfamily)
+                .set(AMMONITE.TAXONOMY_GENUS, ammonite.taxonomyGenus)
+                .set(AMMONITE.TAXONOMY_SUBGENUS, ammonite.taxonomySubgenus)
+                .set(AMMONITE.TAXONOMY_SPECIES, ammonite.taxonomySpecies)
+                .set(AMMONITE.DESCRIPTION, ammonite.description)
+                .set(AMMONITE.STRATA, ammonite.strata)
+                .where(AMMONITE.ID.eq(ammonite.id))
+                .execute()
+            return ammonite
+        }
+    }
+
+    fun deleteById(id: Int) {
+        dsl.deleteFrom(AMMONITE).where(AMMONITE.ID.eq(id)).execute()
+    }
+
+    data class AmmoniteWithMeasurements (val ammonite: Ammonite, val measurement: Measurement)
+
+    fun findAllWithMeasurements(): List<AmmoniteWithMeasurements> {
+        return dsl.select()
+            .from(AMMONITE)
+            .join(Tables.MEASUREMENT).on(AMMONITE.ID.eq(Tables.MEASUREMENT.AMMONITE_ID))
+            .fetch()
+            .map { record ->
+                val ammonite = record.into(Ammonite::class.java)
+                val measurement = record.into(Measurement::class.java)
+                AmmoniteWithMeasurements(ammonite, measurement)
+            }
+    }
+
+}
